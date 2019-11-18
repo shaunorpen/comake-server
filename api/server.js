cca9628b21499a3dfa6ad15f2805c5bcfb3a82d7
shaunorpen <shaunorpen@users.noreply.github.com>
2019-11-18 15:03:58 +0000

    Add sessions

--- api/server.js
+++ api/server.js
@@ -1,14 +1,23 @@
-const express = require("express");
 const cors = require("cors");
+const express = require("express");
 const helmet = require("helmet");
+const session = require("express-session");
 
 const usersRouter = require("../users/usersRouter");
 
+const sessionConfig = {
+  secret: "keyboard cat",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+};
+
 const server = express();
 
-server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(helmet());
+server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
 
